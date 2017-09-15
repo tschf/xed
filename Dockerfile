92fcca5bff988e4df686f6a1bf7c030a1402f4e0
Trent Schafer <trent.schafer@gmail.com>
2017-09-14 22:01:46 -0400

    Flesh out oracle install script

--- Dockerfile
+++ Dockerfile
@@ -12,12 +12,16 @@ MAINTAINER Trent Schafer <trent.schafer@gmail.com>
 # $HOME seems to come back as `/`. 
 USER root
 WORKDIR /root
-ENV HOME /root
-
+ENV HOME /root 
+ENV USER root
 
 ENV INSTALL_DIR="install" \
     PATH="$HOME/go/bin:$PATH" \
-    GOPATH="$HOME/go"
+    GOPATH="$HOME/go" \
+    ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe \
+    ORACLE_SID=XE
+
+ENV PATH="$ORACLE_HOME/bin:$PATH"
 
 COPY install/ $INSTALL_DIR/
 
@@ -27,6 +31,7 @@ RUN apt-get -qq update && \
     apt-get install -y --no-install-recommends $(cat $INSTALL_DIR/requirements.txt) > /dev/null
 
 RUN mkdir /var/lock/subsys && \
-    mkdir $HOME/go
+    mkdir $HOME/go && \
+    ln -s /usr/bin/awk /bin/awk
 
 RUN go get github.com/tschf/odl
--- install/installXe.sh
+++ install/installXe.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Expect one arg, which is the path to the xe.rpm.zip file
 XE_RPM_ZIP=$1
+XE_CONF_INPUT=$HOME/install/xe-conf-input.txt
 
 # Move into /tmp to run the installation
 mkdir -p /tmp/install
-cp ${XE_RPM_ZIP} /tmp/install/
+cp ${XE_RPM_ZIP} ${XE_CONF_INPUT} /tmp/install/
 cd /tmp/install
 
 XE_RPM_BASE_ZIP="$(basename "${XE_RPM_ZIP}")"
@@ -13,3 +14,13 @@ XE_RPM="$(basename "${XE_RPM_BASE_ZIP}" .zip)"
 unzip -j ${XE_RPM_BASE_ZIP} "Disk1/${XE_RPM}"
 rpm --install --nodeps --nopre "${XE_RPM}"
 
+# https://www.stigviewer.com/stig/oracle_database_11g_instance/2015-03-26/finding/V-2531
+# Set to something other than default of OP$
+echo 'OS_AUTHENT_PREFIX=""' | tee -a "$ORACLE_HOME/config/scripts/init.ora" > /dev/null
+
+/etc/init.d/oracle-xe configure < xe-conf-input.txt
+
+sqlplus sys/oracle as sysdba <<SQL
+create user scott identified by tiger;
+grant connect,resource to scott;
+SQL
