fefb9b0688898a749aa6e3dc95a19fbf4d53a717
Trent Schafer <trent.schafer@gmail.com>
2017-09-15 23:31:08 -0400

    Add listener.ora The docker container will always get a different hostname. So set a listener.ora that doesn't depend on a hostname

--- install/installXe.sh
+++ install/installXe.sh
@@ -2,10 +2,11 @@
 # Expect one arg, which is the path to the xe.rpm.zip file
 XE_RPM_ZIP=$1
 XE_CONF_INPUT=$HOME/install/xe-conf-input.txt
+LISTENER_FILE=$HOME/install/listener.ora
 
 # Move into /tmp to run the installation
 mkdir -p /tmp/install
-cp ${XE_RPM_ZIP} ${XE_CONF_INPUT} /tmp/install/
+cp ${XE_RPM_ZIP} ${XE_CONF_INPUT} ${LISTENER_FILE} /tmp/install/
 cd /tmp/install
 
 XE_RPM_BASE_ZIP="$(basename "${XE_RPM_ZIP}")"
@@ -17,6 +18,7 @@ rpm --install --nodeps --nopre "${XE_RPM}"
 # https://www.stigviewer.com/stig/oracle_database_11g_instance/2015-03-26/finding/V-2531
 # Set to something other than default of OP$
 echo 'OS_AUTHENT_PREFIX=""' | tee -a "$ORACLE_HOME/config/scripts/init.ora" > /dev/null
+cp listener.ora $ORACLE_HOME/network/admin/listener.ora
 
 /etc/init.d/oracle-xe configure < xe-conf-input.txt
 
