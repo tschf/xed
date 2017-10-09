eb1106137d5ea1e4235366dfd3dbf1c924f22194
Trent Schafer <trent.schafer@gmail.com>
2017-10-09 16:40:45 -0400

    Add the run file to /tmp/install

--- install/installXe.sh
+++ install/installXe.sh
@@ -3,10 +3,11 @@
 XE_RPM_ZIP=$1
 XE_CONF_INPUT=$HOME/install/xe-conf-input.txt
 LISTENER_FILE=$HOME/install/listener.ora
+RUN_FILE=$HOME/install/runXe.sh
 
 # Move into /tmp to run the installation
 mkdir -p /tmp/install
-cp ${XE_RPM_ZIP} ${XE_CONF_INPUT} ${LISTENER_FILE} /tmp/install/
+cp ${XE_RPM_ZIP} ${XE_CONF_INPUT} ${LISTENER_FILE} ${RUN_FILE} /tmp/install/
 cd /tmp/install
 
 XE_RPM_BASE_ZIP="$(basename "${XE_RPM_ZIP}")"
